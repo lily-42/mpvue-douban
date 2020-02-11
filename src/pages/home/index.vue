dd2c4218fb7b17705b8eb46a1e427b44d7a77239
lily-42 <1471786654@qq.com>
2020-02-11 21:09:03 +0800

    头部

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,13 +1,56 @@
 <template>
-  <div>hello</div>
+  <div class="header">
+    <span>豆瓣</span>
+    <icon type="search" size="16" color="#00b600"></icon>
+    <button>打开豆瓣App</button>
+  </div>
+  <!-- 影院热映 -->
+  <div class="movie-item">
+    <p class="title">
+      <span>影院热映</span>
+      <span class="more-link">更多</span>
+    </p>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="less">
+.header {
+  display: flex;
+  border-bottom: 1rpx solid #eee;
+  align-items: center;
+  padding: 0 36rpx;
+  height: 94rpx;
+  span {
+    color: #00b600;
+    font-size: 40rpx;
+  }
+  icon {
+    flex: 1;
+    margin-left: 28rpx;
+    margin-top: 10rpx;
+  }
+  button {
+    width: 200rpx;
+    height: 58rpx;
+    border: 8rpx;
+    color: #fff;
+    background-color: #42bd56;
+  }
+}
+.movie-item{
+  .title{
+    height: 87rpx;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 36rpx;
+    align-items: center;
+    .more-link{
+      color: #42bd56;
+    }
+  }
+}
 </style>
